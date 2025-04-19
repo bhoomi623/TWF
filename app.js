9edf1aa6b2564eb723057f83ceb54b25f4c5d565
bhoomi623 <bhoomi3051@gmail.com>
2025-04-19 21:41:04 +0530

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express();
-
-
 app.use(bodyParser.json());
-
 
 const distance = {
   C1: { L1: 10 },
@@ -14,54 +10,38 @@ const distance = {
   C3: { L1: 30 }
 };
 
-const costPerKm = 5; 
-
+const costPerKm = 5;
 
 const calculateCost = (order) => {
   let totalCost = 0;
-  let totalDistance = 0;
+  let deliveryCost = 0;
 
-  
   for (let product in order) {
-    totalCost += order[product] * 10; 
+    totalCost += order[product] * 10;
   }
 
-  
-  let bestDeliveryCost = Infinity;
-
-  
   if (order.A || order.B || order.C) {
-    
-    totalDistance = distance.C1.L1;
-    bestDeliveryCost = Math.min(bestDeliveryCost, totalDistance * costPerKm);
+    deliveryCost += distance.C1.L1 * costPerKm;
   }
 
   if (order.D || order.E || order.F) {
-    
-    totalDistance = distance.C2.L1;
-    bestDeliveryCost = Math.min(bestDeliveryCost, totalDistance * costPerKm);
+    deliveryCost += distance.C2.L1 * costPerKm;
   }
 
   if (order.G || order.H || order.I) {
-    
-    totalDistance = distance.C3.L1;
-    bestDeliveryCost = Math.min(bestDeliveryCost, totalDistance * costPerKm);
+    deliveryCost += distance.C3.L1 * costPerKm;
   }
 
-  
-  totalCost += bestDeliveryCost;
-
-  return totalCost;
+  return totalCost + deliveryCost;
 };
 
-
 app.post('/calculate-delivery-cost', (req, res) => {
-  const order = req.body;  
-  const totalCost = calculateCost(order);  
-  res.json({ deliveryCost: totalCost });  
+  const order = req.body;
+  const totalCost = calculateCost(order);
+  res.json({ deliveryCost: totalCost });
 });
 
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
